174a73d6ec9c7281ec1a4a2a8f70daad90b73f4c
Darrry <2997266712@qq.com>
2018-08-21 18:00:08 +0800

    SpringBoot

---
+++ demo1/src/main/java/com/dairui/sxwl/demo1/Demo1Application.java
@@ -0,0 +1,16 @@
+package com.dairui.sxwl.demo1;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Demo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+    }
+
+}
